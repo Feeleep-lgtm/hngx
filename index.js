142533aa9d858a50f65f61d3fdab6eca4a14e9e1
feeleep <aliup45@gmail.com>
2023-09-09 10:40:23 +0100

    Commit changes to index.js

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 const express = require('express');
 
 const app = express();
-
+const aqp = require("api-query-params")
 const cors = require("cors");
 
 app.use(express.json());
 
 
-const PORT = process.env.PORT || s3000;
+const PORT = process.env.PORT || 3000;
 let day = new Date()
 const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 currentDay = weekday[day.getDay()]
 let utc = new Date().getUTCDate
 
-app.get('/api', (req, res) =>{
+app.get('/api?', async (req, res) =>{
    
-    
+    let track = req.query.track
+    let slack_name = req.query.slack_name
+   
     console.log(day);
     res.status(200).json({
-        "slack_name": 'Feeleep',
+        "slack_name": slack_name,
         "current_day": currentDay,
         "utc_time": utc,
         "github_repo_url": "https://github.com/Feeleep-lgtm/hngx",
-        "track": "backend",
+        "track": track,
         "github_file_url": "https://github.com/Feeleep-lgtm/hngx/blob/main/index.js",
         "status_code": "200"
 
