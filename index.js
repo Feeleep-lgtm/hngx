9ac25c0361316584c281fec6117bdb1f5eddd543
feeleep <aliup45@gmail.com>
2023-09-09 14:38:08 +0100

    commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 //const aqp = require("api-query-params")
 const cors = require("cors");
 const moment = require('moment');
+const datetime = require('datetime')
 
 app.use(express.json());
 
@@ -15,9 +16,10 @@ const day = new Date()
 // currentDay = weekday[day.getDay()]
 // let utc = day.toISOString().slice(0, 19) + "Z"
 const currentDay = day.toLocaleString('en-US', {weekday: 'long'});
- const currentDate =moment.utc().format("YYYY-MM-DDTHH:mm:ss[Z]")
+// const currentDate =moment.utc().format("YYYY-MM-DDTHH:mm:ss[Z]")
 //const currentDate = new Date(Date.now()).toISOString().split('.')[0] + 'Z'
 // console.log(utc);
+const now = new Date().toISOString().slice(0, 19) + 'Z';
 
 app.get('/api', async (req, res) =>{
    
@@ -27,7 +29,7 @@ app.get('/api', async (req, res) =>{
     res.status(200).json({
         "slack_name": slack_name,
         "current_day": currentDay,
-        "utc_time": currentDate,
+        "utc_time": now,//datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ'),
         "github_repo_url": "https://github.com/Feeleep-lgtm/hngx",
         "track": track,
         "github_file_url": "https://github.com/Feeleep-lgtm/hngx/blob/main/index.js",
